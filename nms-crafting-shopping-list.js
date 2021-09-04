cda504479fb5c9305822d3254ea07230e24167c2
Jason Milkins <jasonm23@gmail.com>
2021-09-05 00:35:45 +0700

    Split mined/harvested and crafted into / shopping list and bill of materials

--- nms-crafting-shopping-list.js
+++ nms-crafting-shopping-list.js
@@ -2,292 +2,392 @@
 var craftingData = [
   {"name":"Stasis Device","value":"15,600,000",
    "resources":[
-     {"name":"Quantum Processor","qty":"1"},
-     {"name":"Cryogenic Chamber","qty":"1"},
-     {"name":"Iridesite","qty":"1"}]},
+     {"name":"Quantum Processor","qty": 1},
+     {"name":"Cryogenic Chamber","qty": 1},
+     {"name":"Iridesite","qty": 1}]},
   {"name":"Fusion Ignitor","value":"15,600,000",
    "resources":[
-     {"name":"Quantum Processor","qty":"1"},
-     {"name":"Portable Reactor","qty":"1"},
-     {"name":"Geodesite","qty":"1"}]},
+     {"name":"Quantum Processor","qty": 1},
+     {"name":"Portable Reactor","qty": 1},
+     {"name":"Geodesite","qty": 1}]},
   {"name":"Quantum Processor","value":"5,200,000",
    "resources":[
-     {"name":"Circuit Board","qty":"1"},
-     {"name":"Superconductor","qty":"1"}]},
+     {"name":"Circuit Board","qty": 1},
+     {"name":"Superconductor","qty": 1}]},
   {"name":"Portable Reactor","value":"4,200,000",
    "resources":[
-     {"name":"Liquid Explosive","qty":"1"},
-     {"name":"Fusion Accelerant","qty":"1"}]},
+     {"name":"Liquid Explosive","qty": 1},
+     {"name":"Fusion Accelerant","qty": 1}]},
   {"name":"Cryogenic Chamber","value":"3,800,000",
    "resources":[
-     {"name":"Living Glass","qty":"1"},
-     {"name":"Cryo-Pump","qty":"1"}]},
+     {"name":"Living Glass","qty": 1},
+     {"name":"Cryo-Pump","qty": 1}]},
   {"name":"Superconductor","value":"2,000,000",
    "resources":[
-     {"name":"Semiconductor","qty":"1"},
-     {"name":"Enriched Carbon","qty":"1"}]},
+     {"name":"Semiconductor","qty": 1},
+     {"name":"Enriched Carbon","qty": 1}]},
   {"name":"Cryo-Pump","value":"1,500,000",
    "resources":[
-     {"name":"Thermic Condensate","qty":"1"},
-     {"name":"Hot Ice","qty":"1"}]},
+     {"name":"Thermic Condensate","qty": 1},
+     {"name":"Hot Ice","qty": 1}]},
   {"name":"Fusion Accelerant","value":"1,500,000",
    "resources":[
-     {"name":"Organic Catalyst","qty":"1"},
-     {"name":"Nitrogen Salt","qty":"1"}]},
+     {"name":"Organic Catalyst","qty": 1},
+     {"name":"Nitrogen Salt","qty": 1}]},
   {"name":"Circuit Board","value":"916,250",
    "resources":[
-     {"name":"Poly Fibre","qty":"1"},
-     {"name":"Heat Capacitor","qty":"1"}]},
+     {"name":"Poly Fibre","qty": 1},
+     {"name":"Heat Capacitor","qty": 1}]},
   {"name":"Liquid Explosive","value":"800,500",
    "resources":[
-     {"name":"Acid","qty":"1"},
-     {"name":"Unstable Gel","qty":"1"}]},
+     {"name":"Acid","qty": 1},
+     {"name":"Unstable Gel","qty": 1}]},
   {"name":"Living Glass","value":"566,000",
    "resources":[
-     {"name":"Glass","qty":"5"},
-     {"name":"Lubricant","qty":"1"}]},
+     {"name":"Glass","qty": 5},
+     {"name":"Lubricant","qty": 1}]},
   {"name":"Semiconductor","value":"400,000",
    "resources":[
-     {"name":"Nitrogen Salt","qty":"1"},
-     {"name":"Thermic Condensate","qty":"1"}]},
+     {"name":"Nitrogen Salt","qty": 1},
+     {"name":"Thermic Condensate","qty": 1}]},
   {"name":"Hot Ice","value":"400,000",
    "resources":[
-     {"name":"Enriched Carbon","qty":"1"},
-     {"name":"Nitrogen Salt","qty":"1"}]},
+     {"name":"Enriched Carbon","qty": 1},
+     {"name":"Nitrogen Salt","qty": 1}]},
   {"name":"Organic Catalyst","value":"320,000",
    "resources":[
-     {"name":"Thermic Condensate","qty":"1"},
-     {"name":"Enriched Carbon","qty":"1"}]},
+     {"name":"Thermic Condensate","qty": 1},
+     {"name":"Enriched Carbon","qty": 1}]},
   {"name":"Acid","value":"188,000",
    "resources":[
-     {"name":"Mordite","qty":"25"},
-     {"name":"Fungal Mould","qty":"600"}]},
+     {"name":"Mordite","qty": 25},
+     {"name":"Fungal Mould","qty": 600}]},
   {"name":"Heat Capacitor","value":"180,000",
    "resources":[
-     {"name":"Solanium","qty":"200"},
-     {"name":"Frost Crystal","qty":"100"}]},
+     {"name":"Solanium","qty": 200},
+     {"name":"Frost Crystal","qty": 100}]},
   {"name":"Grantine","value":"150,000",
    "resources":[
-     {"name":"Ionised Cobalt","qty":"50"},
-     {"name":"Dioxite","qty":"50"}]},
+     {"name":"Ionised Cobalt","qty": 50},
+     {"name":"Dioxite","qty": 50}]},
   {"name":"Geodesite","value":"150,000",
    "resources":[
-     {"name":"Dirty Bronze","qty":"1"},
-     {"name":"Herox","qty":"1"},
-     {"name":"Lemmium","qty":"1"}]},
+     {"name":"Dirty Bronze","qty": 1},
+     {"name":"Herox","qty": 1},
+     {"name":"Lemmium","qty": 1}]},
   {"name":"Iridesite","value":"150,000",
    "resources":[
-     {"name":"Aronium","qty":"1"},
-     {"name":"Magno-Gold","qty":"1"},
-     {"name":"Grantine","qty":"1"}]},
+     {"name":"Aronium","qty": 1},
+     {"name":"Magno-Gold","qty": 1},
+     {"name":"Grantine","qty": 1}]},
   {"name":"Poly Fibre","value":"130,000",
    "resources":[
-     {"name":"Cactus Flesh","qty":"100"},
-     {"name":"Star Bulb","qty":"200"}]},
+     {"name":"Cactus Flesh","qty": 100},
+     {"name":"Star Bulb","qty": 200}]},
   {"name":"Lubricant","value":"110,000",
    "resources":[
-     {"name":"Faecium","qty":"50"},
-     {"name":"Gamma Root","qty":"400"}]},
+     {"name":"Faecium","qty": 50},
+     {"name":"Gamma Root","qty": 400}]},
   {"name":"Nitrogen Salt","value":"50,000",
    "resources":[
-     {"name":"Condensed Carbon","qty":"50"},
-     {"name":"Radon","qty":"250"}]},
+     {"name":"Condensed Carbon","qty": 50},
+     {"name":"Radon","qty": 250}]},
   {"name":"Enriched Carbon","value":"50,000",
    "resources":[
-     {"name":"Condensed Carbon","qty":"50"},
-     {"name":"Radon","qty":"250"}]},
+     {"name":"Condensed Carbon","qty": 50},
+     {"name":"Radon","qty": 250}]},
   {"name":"Thermic Condensate","value":"50,000",
    "resources":[
-     {"name":"Condensed Carbon","qty":"50"},
-     {"name":"Sulphurine","qty":"250"}]},
+     {"name":"Condensed Carbon","qty": 50},
+     {"name":"Sulphurine","qty": 250}]},
   {"name":"Unstable Gel","value":"50,000",
    "resources":[
-     {"name":"Cactus Flesh","qty":"200"}]},
+     {"name":"Cactus Flesh","qty": 200}]},
   {"name":"Warp Cell","value":"46,750",
    "resources":[
-     {"name":"Antimatter Housing","qty":"1"},
-     {"name":"Antimatter","qty":"1"}]},
+     {"name":"Antimatter Housing","qty": 1},
+     {"name":"Antimatter","qty": 1}]},
   {"name":"Warp Hypercore","value":"46,750",
    "resources":[
-     {"name":"Antimatter","qty":"1"},
-     {"name":"Storm Crystal","qty":"1"}]},
+     {"name":"Antimatter","qty": 1},
+     {"name":"Storm Crystal","qty": 1}]},
   {"name":"Salt Refractor","value":"30,500",
    "resources":[
-     {"name":"Galactic Trade Terminal","qty":"1"}]},
+     {"name":"Galactic Trade Terminal","qty": 1}]},
   {"name":"Lemmium","value":"25,000",
    "resources":[
-     {"name":"Uranium","qty":"1"},
-     {"name":"Pure Ferrite","qty":"1"}]},
+     {"name":"Uranium","qty": 1},
+     {"name":"Pure Ferrite","qty": 1}]},
   {"name":"Dirty Bronze","value":"25,000",
    "resources":[
-     {"name":"Pyrite","qty":"1"},
-     {"name":"Pure Ferrite","qty":"1"}]},
+     {"name":"Pyrite","qty": 1},
+     {"name":"Pure Ferrite","qty": 1}]},
   {"name":"Magno-Gold","value":"25,000",
    "resources":[
-     {"name":"Ionised Cobalt","qty":"50"},
-     {"name":"Phosphorus","qty":"1"}]},
+     {"name":"Ionised Cobalt","qty": 50},
+     {"name":"Phosphorus","qty": 1}]},
   {"name":"Aronium","value":"25,000",
    "resources":[
-     {"name":"Ionised Cobalt","qty":"50"},
-     {"name":"Paraffinium","qty":"50"}]},
+     {"name":"Ionised Cobalt","qty": 50},
+     {"name":"Paraffinium","qty": 50}]},
   {"name":"Herox","value":"25,000",
    "resources":[
-     {"name":"Ammonia","qty":"1"},
-     {"name":"Ionised Cobalt","qty":"1"}]},
+     {"name":"Ammonia","qty": 1},
+     {"name":"Ionised Cobalt","qty": 1}]},
   {"name":"Cobalt Mirror","value":"20,500",
    "resources":[
-     {"name":"Galactic Trade Terminal","qty":"1"}]},
+     {"name":"Galactic Trade Terminal","qty": 1}]},
   {"name":"Amino Chamber","value":"12,300",
    "resources":[
-     {"name":"Metal Plating","qty":"1"},
-     {"name":"Chlorine","qty":"20"},
-     {"name":"Condensed Carbon","qty":"25"}]},
+     {"name":"Metal Plating","qty": 1},
+     {"name":"Chlorine","qty": 20},
+     {"name":"Condensed Carbon","qty": 25}]},
   {"name":"Antimatter Housing","value":"6,500",
    "resources":[
-     {"name":"Oxygen","qty":"30"},
-     {"name":"Ferrite Dust","qty":"50"}]},
+     {"name":"Oxygen","qty": 30},
+     {"name":"Ferrite Dust","qty": 50}]},
   {"name":"Magnetic Resonator","value":"6,150",
-   "resources":[{"name":"Magnetised Ferrite","qty":"40"},
-                {"name":"Ionised Cobalt","qty":"40"}]},
+   "resources":[{"name":"Magnetised Ferrite","qty": 40},
+                {"name":"Ionised Cobalt","qty": 40}]},
   {"name":"Solar Mirror","value":"6,150",
    "resources":[
-     {"name":"Gold","qty":"40"},
-     {"name":"Silver","qty":"30"},
-     {"name":"Chromatic Metal","qty":"25"}]},
+     {"name":"Gold","qty": 40},
+     {"name":"Silver","qty": 30},
+     {"name":"Chromatic Metal","qty": 25}]},
   {"name":"Antimatter","value":"5233",
    "resources":[
-     {"name":"Chromatic Metal","qty":"25"},
-     {"name":"Condensed Carbon","qty":"20"}]},
+     {"name":"Chromatic Metal","qty": 25},
+     {"name":"Condensed Carbon","qty": 20}]},
   {"name":"Quantum Computer","value":"4,200",
    "resources":[
-     {"name":"Microprocessor","qty":"1"},
-     {"name":"Antimatter","qty":"1"},
-     {"name":"Chromatic Metal","qty":"25"}]},
+     {"name":"Microprocessor","qty": 1},
+     {"name":"Antimatter","qty": 1},
+     {"name":"Chromatic Metal","qty": 25}]},
   {"name":"Hydraulic Wiring","value":"3,600",
    "resources":[
-     {"name":"Carbon Nanotubes","qty":"2"},
-     {"name":"Salt","qty":"20"},
-     {"name":"Di-hydrogen","qty":"40"}]},
+     {"name":"Carbon Nanotubes","qty": 2},
+     {"name":"Salt","qty": 20},
+     {"name":"Di-hydrogen","qty": 40}]},
   {"name":"Sodium Diode","value":"3,500",
    "resources":[
-     {"name":"Galactic Trade Terminal","qty":"1"}]},
+     {"name":"Galactic Trade Terminal","qty": 1}]},
   {"name":"AtlasPass v3","value":"2,613",
    "resources":[
-     {"name":"Emeril","qty":"200"},
-     {"name":"Microprocessor","qty":"1"}]},
+     {"name":"Emeril","qty": 200},
+     {"name":"Microprocessor","qty": 1}]},
   {"name":"Microprocessor","value":"2,000",
    "resources":[
-     {"name":"Chromatic Metal","qty":"40"},
-     {"name":"Carbon Nanotubes","qty":"1"}]},
+     {"name":"Chromatic Metal","qty": 40},
+     {"name":"Carbon Nanotubes","qty": 1}]},
   {"name":"AtlasPass v2","value":"1,856",
    "resources":[
-     {"name":"Cadmium","qty":"200"},
-     {"name":"Microprocessor","qty":"1"}]},
+     {"name":"Cadmium","qty": 200},
+     {"name":"Microprocessor","qty": 1}]},
   {"name":"AtlasPass v1","value":"825",
    "resources":[
-     {"name":"Copper","qty":"200"},
-     {"name":"Microprocessor","qty":"1"}]},
+     {"name":"Copper","qty": 200},
+     {"name":"Microprocessor","qty": 1}]},
   {"name":"Hermetic Seal","value":"800",
    "resources":[
-     {"name":"Condensed Carbon","qty":"30"}]},
+     {"name":"Condensed Carbon","qty": 30}]},
   {"name":"Oxygen Filter","value":"615",
    "resources":[
-     {"name":"Galactic Trade Terminal","qty":"1"}]},
+     {"name":"Galactic Trade Terminal","qty": 1}]},
   {"name":"Carbon Nanotubes","value":"500",
    "resources":[
-     {"name":"Carbon","qty":"50"}]},
+     {"name":"Carbon","qty": 50}]},
   {"name":"Glass","value":"200",
    "resources":[
-     {"name":"Frost Crystal","qty":"40"}]},
+     {"name":"Frost Crystal","qty": 40}]},
   {"name":"Di-hydrogen Jelly","value":"200",
    "resources":[
-     {"name":"Di-hydrogen","qty":"40"}]},
+     {"name":"Di-hydrogen","qty": 40}]},
   {"name":"Ion Battery","value":"200",
    "resources":[
-     {"name":"Ferrite Dust","qty":"20"},{"name":"Cobalt","qty":"10"}]},
+     {"name":"Ferrite Dust","qty": 20},{"name":"Cobalt","qty": 10}]},
   {"name":"Life Support Gel","value":"200",
    "resources":[
-     {"name":"Di-hydrogen Jelly","qty":"1"},{"name":"Carbon","qty":"20"}]}];
+     {"name":"Di-hydrogen Jelly","qty": 1},{"name":"Carbon","qty": 20}]}];
+
+let minedAndCultivated = ['Ammonia',
+                          'Cactus Flesh',
+                          'Cadmium',
+                          'Carbon',
+                          'Chlorine',
+                          'Chromatic Metal',
+                          'Cobalt',
+                          'Condensed Carbon',
+                          'Copper',
+                          'Di-hydrogen',
+                          'Dioxite',
+                          'Emeril',
+                          'Faecium',
+                          'Ferrite Dust',
+                          'Frost Crystal',
+                          'Fungal Mould',
+                          'Galactic Trade Terminal',
+                          'Gamma Root',
+                          'Gold',
+                          'Ionised Cobalt',
+                          'Magnetised Ferrite',
+                          'Metal Plating',
+                          'Mordite',
+                          'Oxygen',
+                          'Paraffinium',
+                          'Phosphorus',
+                          'Pure Ferrite',
+                          'Pyrite',
+                          'Radon',
+                          'Salt',
+                          'Silver',
+                          'Solanium',
+                          'Star Bulb',
+                          'Storm Crystal',
+                          'Sulphurine',
+                          'Uranium'];
 
 function init() {
   let params = new URL(document.location).searchParams;
   let item = params.get("item");
 
   if (searchFor(item)) {
-    displayShoppingList(item);
+    shoppingList(item);
   } else {
-    listAll();
+    index();
   }
 }
 
-function listAll(data = craftingData) {
+// data logic
+
+function searchFor(item, data = craftingData) {
+  var found = data.find((product, index) => product.name == item);
+  return found;
+}
+
+// Presentation section:
+
+// Pages
+
+function index(data = craftingData) {
   var headings = ["<b>Item</b>", "<b>Value</b>"];
-  tableRow(headings);
+  addTableHeading(headings);
 
   data.forEach( row => {
     var {name, value} = row;
     var cells = [`<a href="/?item=${name}">${name}</a>`, value];
-    tableRow(cells);
+    addTableRow(cells);
   });
 }
 
-function displayShoppingList(item) {
-  var container = addContainer(document.body);
-
+function shoppingList(item) {
   var found = searchFor(item);
-  addHeading(`${item} <sup>${found.value}u</sup>`, container);
-  addResourcesTable(found.resources, container);
+  displayElement(`${item} : ${found.value}u`, "h1");
+  displayElement("Shopping List...", "h2");
+  displayElement(`Mine / harvest the materials from the <a href="#bom">bill of materials</a>. Then craft these components in order (last first). After that you can craft the ${item}`, "p");
 
-  presentCraftingResources(found.resources, container);
+  let componentTable = displayResourcesTable(null, document.body, "Component", "Quantity");
+
+  showResources(found.resources, componentTable);
+
+  displayElement(`<a name="bom"></a>`);
+
+  showBillOfMaterials(item);
 }
 
-function presentCraftingResources(resources, container) {
+function showResources(resources, table) {
   resources.forEach(
     (resource) => {
       var found = searchFor(resource.name);
       if(found) {
-        var box = addContainer(container);
-        addHeading(`${resource.name} x ${resource.qty}` , box);
-        addResourcesTable(found.resources, box);
-        presentCraftingResources(found.resources, box);
+        addTableRow([resource.name, resource.qty], table);
+        showResources(found.resources, table);
       };
     }
   );
 }
 
-// Build a shopping list for item.
-// {
-//  name: "",
-//   
-// }
-function selectResourcesFor(item) {
+function getResources(resources, collection = []) {
+  resources?.forEach(
+    (resource) => {
+        collection.push(resource);
+        var found = searchFor(resource?.name);
+        return getResources(found?.resources, collection);
+    }
+  );
 
+  return collection;
 }
 
-function searchFor(item, data = craftingData) {
-  var found = data.find((product, index) => product.name == item);
-  console.log(found);
-  return found;
+function sortResouces(a, b) {
+  return a.name < b.name ? -1 : 1;
 }
 
-function addHeading(text, container = document.body) {
-  var h1 = document.createElement("h1");
-  h1.innerHTML = text;
-  container.appendChild(h1);
+function reducedResouces(resources) {
+  return resources.reduce((p, c) => {
+      let i = p?.map(a => a.name).indexOf(c.name);
+
+      if(i > -1) {
+        p[i].qty += c.qty;
+      } else {
+        p?.push(c);
+      }
+
+      return p;
+    }, []);
 }
 
-function addResourcesTable(items, container = document.body) {
+function filterOnMinedAndCultivated(resources) {
+  return resources.filter( (r) => minedAndCultivated.includes(r.name));
+}
+
+function getSortedResources(item) {
+  return getResources(searchFor(item).resources).sort(sortResouces);
+}
+
+function showBillOfMaterials(item){
+  displayElement(`<a name="bom"></a> ${item} - Bill of Materials`, "h2");
+  displayElement(`Mine / harvest these materials to craft all the components of the ${item}`, "p");
+  displayResourcesTable(filterOnMinedAndCultivated(reducedResouces(getSortedResources(item))));
+}
+
+// html helpers
+function displayElement(text, htmlElementName = "p", container = document.body) {
+  var element = document.createElement(htmlElementName);
+  element.innerHTML = text;
+  container.appendChild(element);
+  return element;
+}
+
+function displayHeading(text, container = document.body) {
+  return displayElement(text, "h1", container);
+}
+
+function displayResourcesTable(items, container = document.body, column1Heading = "Component", column2Heading = "Quantity") {
   var table = document.createElement("table");
   container.appendChild(table);
 
-  tableRow([`<b>Component</b>`, `<b>Quantity</b>`], table);
+  addTableHeading([`<b>${column1Heading}</b>`, `<b>${column2Heading}</b>`], table);
 
-  items.forEach((i) => {
-    if(i.name && i.qty) tableRow([i.name, i.qty], table);
-  });
+  items?.forEach((i) => addTableRow([i.name, i.qty], table));
+
+  return table;
 }
 
-function tableRow(cells, table = document.getElementById("crafting")) {
+function addTableHeading(cells, table = document.getElementById("crafting")) {
+  var thead = document.createElement("thead");
+  var tr = document.createElement("tr");
+
+  cells.forEach((c) => {
+    var th = document.createElement("th");
+    th.innerHTML = c;
+    tr.appendChild(th);
+  });
+
+  thead.appendChild(tr);
+  table.appendChild(thead);
+}
+
+function addTableRow(cells, table = document.getElementById("crafting")) {
   var tr = document.createElement("tr");
 
   cells.forEach((c) => {
@@ -297,12 +397,4 @@ function tableRow(cells, table = document.getElementById("crafting")) {
   });
 
   table.appendChild(tr);
-}
-
-function addContainer(parent, className = "box") {
-  console.log(parent, className);
-  var container = document.createElement('div');
-  container.className = className;
-  parent.appendChild(container);
-  return container;
 }
